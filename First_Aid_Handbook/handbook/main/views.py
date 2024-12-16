21f6ffedf7bfa15ee681f31b67856956141d19fe
Бекболот <82055231+Bekbolot2804@users.noreply.github.com>
2024-12-16 23:13:19 +0300

    1

--- First_Aid_Handbook/handbook/main/views.py
+++ First_Aid_Handbook/handbook/main/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate
 from .models import Help, Lesion, HelpLesion
@@ -20,8 +21,11 @@ class HelpView(APIView):
     def post(self, request):
         serializer = HelpSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            try:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except IntegrityError as e:
+                return Response({"error": "Integrity Error: Duplicate or invalid data"}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, id):
