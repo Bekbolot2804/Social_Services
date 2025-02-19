5a634dae1a33f7352c339a04aaa90cc14fb0ca40
Бекболот <82055231+Bekbolot2804@users.noreply.github.com>
2025-02-19 22:33:41 +0300

    Reapply "ну это просто ужасс!!!"
    This reverts commit 56cf8f3d5410581de48a400ca74799d775096184.

--- First_Aid_Handbook/handbook/handbook/settings.py
+++ First_Aid_Handbook/handbook/handbook/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'handbook',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -127,7 +128,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+REDIS_HOST = '0.0.0.0'
+REDIS_PORT = 6379
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -143,5 +145,14 @@ AWS_ACCESS_KEY_ID = 'beka'
 AWS_SECRET_ACCESS_KEY = '20042804'
 AWS_S3_ENDPOINT_URL = 'localhost:9000'
 MINIO_USE_SSL = False
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
 AUTH_USER_MODEL = 'main.CustomUser'  # Должно совпадать с именем модели
--- First_Aid_Handbook/handbook/handbook/urls.py
+++ First_Aid_Handbook/handbook/handbook/urls.py
@@ -1,15 +1,46 @@
-from django.urls import path
+from django.urls import path, include
 from main import views
+from rest_framework import routers  # Импортируем модуль routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from main.permissions import permissions
+from main.views import UserViewSet
+
+# Создаем экземпляр DefaultRouter или SimpleRouter
+router = routers.DefaultRouter()  # Используем DefaultRouter
+
+# Регистрируем viewset в роутере
+router.register(r'user', UserViewSet, basename='user')
+
+# Создаем схему Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    # Роуты для API
+    path('user', include(router.urls)),  # Включаем URL-шаблоны, зарегистрированные в router
+
+    # Другие URL-шаблоны
     path('helps/', views.HelpView.as_view()),
     path('helps/<int:pk>/', views.HelpDetailView.as_view()),
     path('helps/<int:pk>/image/', views.ImageUploadView.as_view()),
-    
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
     path('lesions/', views.LesionView.as_view()),
     path('lesions/<int:pk>/', views.LesionDetailView.as_view()),
     path('lesions/draft/', views.LesionDraftView.as_view()),
-    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('lesions/<int:lesion_id>/helps/', views.HelpLesionView.as_view()),
     path('lesions/<int:lesion_id>/helps/<int:help_id>/', views.HelpLesionView.as_view()),
 ]
